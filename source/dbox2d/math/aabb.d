d086120ec299819acc5df2ec096e1edeb8840f54
vlasovroman96 <vlasovroman.ru@yandex.ru>
2025-08-25 12:21:30 +0700

    Moved b2AABB_RayCast into b2AABB

--- source/dbox2d/math/aabb.d
+++ source/dbox2d/math/aabb.d
@@ -52,125 +52,122 @@ struct b2AABB {
 		valid = valid && b2IsValidVec2( this.lowerBound ) && b2IsValidVec2( this.upperBound );
 		return valid;
 	}
-}
 
-// Ray cast an AABB
-b2CastOutput b2AABB_RayCast(b2AABB a, b2Vec2 p1, b2Vec2 p2);
-
-// From Real-time Collision Detection, p179.
-b2CastOutput b2AABB_RayCast(b2AABB a, b2Vec2 p1, b2Vec2 p2)
-{
-	// Radius not handled
-	b2CastOutput output;
-
-	float tmin = -float.max;
-	float tmax = float.max;
-
-	b2Vec2 p = p1;
-	b2Vec2 d = p2 - p1 ;
-	b2Vec2 absD = d.abs();
-
-	b2Vec2 normal = b2Vec2.zero();
-
-	// x-coordinate
-	if ( absD.x < float.epsilon )
+	// From Real-time Collision Detection, p179.
+	b2CastOutput rayCast(b2Vec2 p1, b2Vec2 p2)
 	{
-		// parallel
-		if ( p.x < a.lowerBound.x || a.upperBound.x < p.x )
+		// Radius not handled
+		b2CastOutput output;
+
+		float tmin = -float.max;
+		float tmax = float.max;
+
+		b2Vec2 p = p1;
+		b2Vec2 d = p2 - p1 ;
+		b2Vec2 absD = d.abs();
+
+		b2Vec2 normal = b2Vec2.zero();
+
+		// x-coordinate
+		if ( absD.x < float.epsilon )
+		{
+			// parallel
+			if ( p.x < this.lowerBound.x || this.upperBound.x < p.x )
+			{
+				return output;
+			}
+		}
+		else
+		{
+			float inv_d = 1.0f / d.x;
+			float t1 = ( this.lowerBound.x - p.x ) * inv_d;
+			float t2 = ( this.upperBound.x - p.x ) * inv_d;
+
+			// Sign of the normal vector.
+			float s = -1.0f;
+
+			if ( t1 > t2 )
+			{
+				float tmp = t1;
+				t1 = t2;
+				t2 = tmp;
+				s = 1.0f;
+			}
+
+			// Push the min up
+			if ( t1 > tmin )
+			{
+				normal.y = 0.0f;
+				normal.x = s;
+				tmin = t1;
+			}
+
+			// Pull the max down
+			tmax = min( tmax, t2 );
+
+			if ( tmin > tmax )
+			{
+				return output;
+			}
+		}
+
+		// y-coordinate
+		if ( absD.y < float.epsilon )
+		{
+			// parallel
+			if ( p.y < this.lowerBound.y || this.upperBound.y < p.y )
+			{
+				return output;
+			}
+		}
+		else
+		{
+			float inv_d = 1.0f / d.y;
+			float t1 = ( this.lowerBound.y - p.y ) * inv_d;
+			float t2 = ( this.upperBound.y - p.y ) * inv_d;
+
+			// Sign of the normal vector.
+			float s = -1.0f;
+
+			if ( t1 > t2 )
+			{
+				float tmp = t1;
+				t1 = t2;
+				t2 = tmp;
+				s = 1.0f;
+			}
+
+			// Push the min up
+			if ( t1 > tmin )
+			{
+				normal.x = 0.0f;
+				normal.y = s;
+				tmin = t1;
+			}
+
+			// Pull the max down
+			tmax = min( tmax, t2 );
+
+			if ( tmin > tmax )
+			{
+				return output;
+			}
+		}
+
+		// Does the ray start inside the box?
+		// Does the ray intersect beyond the max fraction?
+		if ( tmin < 0.0f || 1.0f < tmin )
 		{
 			return output;
 		}
-	}
-	else
-	{
-		float inv_d = 1.0f / d.x;
-		float t1 = ( a.lowerBound.x - p.x ) * inv_d;
-		float t2 = ( a.upperBound.x - p.x ) * inv_d;
 
-		// Sign of the normal vector.
-		float s = -1.0f;
-
-		if ( t1 > t2 )
-		{
-			float tmp = t1;
-			t1 = t2;
-			t2 = tmp;
-			s = 1.0f;
-		}
-
-		// Push the min up
-		if ( t1 > tmin )
-		{
-			normal.y = 0.0f;
-			normal.x = s;
-			tmin = t1;
-		}
-
-		// Pull the max down
-		tmax = min( tmax, t2 );
-
-		if ( tmin > tmax )
-		{
-			return output;
-		}
-	}
-
-	// y-coordinate
-	if ( absD.y < float.epsilon )
-	{
-		// parallel
-		if ( p.y < a.lowerBound.y || a.upperBound.y < p.y )
-		{
-			return output;
-		}
-	}
-	else
-	{
-		float inv_d = 1.0f / d.y;
-		float t1 = ( a.lowerBound.y - p.y ) * inv_d;
-		float t2 = ( a.upperBound.y - p.y ) * inv_d;
-
-		// Sign of the normal vector.
-		float s = -1.0f;
-
-		if ( t1 > t2 )
-		{
-			float tmp = t1;
-			t1 = t2;
-			t2 = tmp;
-			s = 1.0f;
-		}
-
-		// Push the min up
-		if ( t1 > tmin )
-		{
-			normal.x = 0.0f;
-			normal.y = s;
-			tmin = t1;
-		}
-
-		// Pull the max down
-		tmax = min( tmax, t2 );
-
-		if ( tmin > tmax )
-		{
-			return output;
-		}
-	}
-
-	// Does the ray start inside the box?
-	// Does the ray intersect beyond the max fraction?
-	if ( tmin < 0.0f || 1.0f < tmin )
-	{
+		// Intersection.
+		output.fraction = tmin;
+		output.normal = normal;
+		output.point = b2Lerp( p1, p2, tmin );
+		output.hit = true;
 		return output;
 	}
-
-	// Intersection.
-	output.fraction = tmin;
-	output.normal = normal;
-	output.point = b2Lerp( p1, p2, tmin );
-	output.hit = true;
-	return output;
 }
 
 /// Does a fully contain b
